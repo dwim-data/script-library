009c454a494b45f14a26a8ab0ad2945314e2c03e
Eric LeVin <eric@ericlevin.me>
2024-05-04 12:02:11 -0500

    added git_info

--- repo_utils/git_utils.py
+++ repo_utils/git_utils.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 from repo_utils.shared import commander
 from repo_utils.repo_folders import repo_dir
@@ -22,3 +23,28 @@ class GitUtils:
         files = commander.exec_system_command('git status --porcelain', folder, log_level=log_level).strip().split('\n')
         files = [ file.strip().replace('M ','').strip() for file in files if len(file.strip()) > 0 ]
         return files
+    
+class GitInfo:
+    def __init__(self, branch : str, commit_date : str, commit_hash : str, commit_hash_short):
+        self.branch = branch
+        self.commit_date = commit_date
+        self.commit_hash = commit_hash
+        self.commit_hash_short = commit_hash_short
+
+    def __str__(self):
+     return f'branch=[{self.branch}], commit_date=[{self.commit_date}], commit_hash_short=[{self.commit_hash_short}], commit_hash=[{self.commit_hash}]'
+
+class GitInfoArgs:
+    def add_git_args(parser : argparse.ArgumentParser):
+        parser.add_argument('--git-branch', default=None, help="The git branch to use, if not, will pull from the git repo")
+        parser.add_argument('--git-commit-date', default=None, help="The git commit date to use, if not, will pull from the git repo")
+        parser.add_argument('--git-commit-hash', default=None, help="The git commit hash to use, if not, will pull from the git repo")
+        parser.add_argument('--git-commit-hash-short', default=None, help="The git commit short hash to use, if not, will pull from the git repo")
+
+    def get_git_info(args: argparse.Namespace, git: GitUtils) -> GitInfo:
+        return GitInfo(
+           branch=args.git_branch if args.git_branch != None else git.get_branch(),
+           commit_date=args.git_commit_date if args.git_commit_date != None else git.get_commit_date(),
+           commit_hash=args.git_commit_hash if args.git_commit_hash != None else git.get_commit_hash(),
+           commit_hash_short=args.git_commit_hash_short if args.git_commit_hash_short != None else git.get_commit_hash_short(),
+        )
